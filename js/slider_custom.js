a814a3506dfe9e1de8acde4764ea20055fabea5b
Miha Oleg <fire913n@gmail.com>
2018-06-18 01:00:32 +0300

    block8

---
+++ js/slider_custom.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){ 
+    
+//Слайдер
+
+  $('.slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      arrows: true,
+      dots: true,
+      speed: 500,
+      fade: true,
+      responsive: [
+          {
+            breakpoint: 750,
+            settings: {
+              arrows: false,
+            }
+          },
+      ]
+  });
+
+  $('.slider2').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      arrows: false,
+      dots: true,
+      speed: 500,
+      fade: true,
+      responsive: [
+          {
+            breakpoint: 750,
+            settings: {
+              dots: true,
+            }
+          },
+      ]
+  });
+
+
+
+});
